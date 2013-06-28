518aa9ca05a746a3dd872cc4073125cf6392bc24
Linzhi.Qi <qilinzhi@gmail.com>
2013-06-28 13:59:00 +0800

    in to int

--- src/stack.cpp
+++ src/stack.cpp
@@ -3,28 +3,28 @@
 
 int main(void)
 {
-    al::stack<int> test_int;
-    test_int.push(1);
-    test_int.push(2);
-    test_int.push(3);
-    test_int.push(4);
-    test_int.pop();
-    test_int.pop();
+    al::stack<int> stack_int;
+    stack_int.push(1);
+    stack_int.push(2);
+    stack_int.push(3);
+    stack_int.push(4);
+    stack_int.pop();
+    stack_int.pop();
 
-    std::cout << "the top elem of stack_int:" << test_int.top() << std::endl;
-    std::cout << "the size of stack_int:" << test_int.size() << std::endl;
+    std::cout << "the top elem of stack_int:" << stack_int.top() << std::endl;
+    std::cout << "the size of stack_int:" << stack_int.size() << std::endl;
 
-    al::stack<double> test_double;
-    test_double.push(1.0);
-    test_double.push(2.0);
-    test_double.push(3.0);
-    test_double.push(4.0);
-    test_double.push(5.0);
-    test_double.pop();
-    test_double.pop();
+    al::stack<double> stack_double;
+    stack_double.push(1.0);
+    stack_double.push(2.0);
+    stack_double.push(3.0);
+    stack_double.push(4.0);
+    stack_double.push(5.0);
+    stack_double.pop();
+    stack_double.pop();
 
-    std::cout << "the top elem of stack_double:" << test_double.top() << std::endl;
-    std::cout << "the size of stack_double:" << test_double.size() << std::endl;
+    std::cout << "the top elem of stack_double:" << stack_double.top() << std::endl;
+    std::cout << "the size of stack_double:" << stack_double.size() << std::endl;
 
     return 0;
 }
